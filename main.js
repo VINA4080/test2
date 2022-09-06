660c1899b35baac88ec7a6cf1ad2b0fb6d7537a9
VINA4080 <vinaandrianarisoa1@gmail.com>
2022-09-06 08:36:39 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// var arrayOne = [2, 8, 9, 10, 13, 14];
+
+// for (let i = 0; i < arrayOne.length; i++) {
+//     arrayOne[i] = arrayOne[i] * 2; 
+// }
+
+// console.table(arrayOne);
+
+// var arr2 = ["a", "b","8", 9, 10, 7, "b"];
+// var arr3 = [];
+
+// for (let i = 0; i < arr2.length; i++) {
+//     // arr3[i] = arr2[i];
+//     if (typeof arr2[i] === "number") {
+//         arr3.push(arr2[i]);
+//     }
+    
+// }
+// console.log(arr3);
+
+// var person = [
+//     {
+//     nom : "Rakoto",
+//     prenom : "Rasoa"
+//     },
+
+//     {
+//     nom : "Rabe",
+//     prenom : "Loha"
+//     },
+
+//     {
+//     nom : "Bema",
+//     prenom : "kely"
+//     },
+// ];
+
+// for (let i = 0; i < person.length; i++) {
+//     var nomEtPrenom = person[i].nom + " " + person[i].prenom;
+//     console.log(nomEtPrenom);
+// }
